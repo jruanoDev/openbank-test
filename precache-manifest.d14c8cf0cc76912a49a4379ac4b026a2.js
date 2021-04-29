9273ee3df154cf8aefeafd94586fe5224d27c449
Jose María Ruano Gálvez <jruanogalvez@gmail.com>
2021-04-29 09:42:56 +0200

    Updates

---
+++ precache-manifest.d14c8cf0cc76912a49a4379ac4b026a2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f8da1a24277a4fc5274b72fff0c558",
+    "url": "/openbank-test/index.html"
+  },
+  {
+    "revision": "21be802a06348d64f3a4",
+    "url": "/openbank-test/static/css/main.f1832a66.chunk.css"
+  },
+  {
+    "revision": "cf74435a52882fdd2a42",
+    "url": "/openbank-test/static/js/2.e0e9da7b.chunk.js"
+  },
+  {
+    "revision": "21be802a06348d64f3a4",
+    "url": "/openbank-test/static/js/main.758263f9.chunk.js"
+  },
+  {
+    "revision": "ff0ecdf364b53dbd9c0f",
+    "url": "/openbank-test/static/js/runtime~main.25fb9028.js"
+  },
+  {
+    "revision": "8bca36b7046880893c45291935907a6b",
+    "url": "/openbank-test/static/media/arrow_right.8bca36b7.svg"
+  },
+  {
+    "revision": "c76c9eb34c76decc0e19933cf7a3d658",
+    "url": "/openbank-test/static/media/checked.c76c9eb3.svg"
+  },
+  {
+    "revision": "9d46f882c2ad6a476b3f9c5a0aab4518",
+    "url": "/openbank-test/static/media/done.9d46f882.svg"
+  },
+  {
+    "revision": "828e06a5791d1c1cb89cd6cd7da78b3a",
+    "url": "/openbank-test/static/media/icon1.828e06a5.svg"
+  },
+  {
+    "revision": "31499ccb464a326d26f3004f9e48772f",
+    "url": "/openbank-test/static/media/icon2.31499ccb.svg"
+  },
+  {
+    "revision": "364e032cf54e13334c2b8dba0c2b0fbe",
+    "url": "/openbank-test/static/media/spinner.364e032c.svg"
+  },
+  {
+    "revision": "5301add17805d480a0219f868ca27bcc",
+    "url": "/openbank-test/static/media/visibility.5301add1.svg"
+  },
+  {
+    "revision": "54810c18fcf370cf703d1925d40628b9",
+    "url": "/openbank-test/static/media/visibility_off.54810c18.svg"
+  },
+  {
+    "revision": "befe209d16ae8b3a00ed2b55cb89a88d",
+    "url": "/openbank-test/static/media/warning.befe209d.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/openbank-test/precache-manifest.d14c8cf0cc76912a49a4379ac4b026a2.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/openbank-test/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
